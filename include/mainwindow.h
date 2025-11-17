df9eafb574eee6d9f4285888f75e0f5826b01d99
Saimon Navarro <navarro.saimon332@gmail.com>
2025-11-16 20:19:50 -0500

    ya se visualiza el welcome en main, pero no las imagenes

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "welcomescreen.h"
 
 namespace Ui {
 class MainWindow;
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "welcomescreen.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //agregamos la pantalla de bienvenida
+    WelcomeScreen *welcomeScreen = new WelcomeScreen(this);
+    //mostrar en el centralwidget
+    setCentralWidget(welcomeScreen);
 }
 
 MainWindow::~MainWindow()
