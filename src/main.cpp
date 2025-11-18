c7b2eb3f2c3457723ed5c170d720cb5e2797a4e1
Saimon Navarro <navarro.saimon332@gmail.com>
2025-11-18 11:43:16 -0500

    diseño de nuevo registro casi completado

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include <QApplication>
 #include "mainwindow.h"
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     MainWindow mainWindow;
-    mainWindow.setWindowTitle("Inventario-de-Autos");
+    mainWindow.setWindowTitle("Inventario de Autos");
     mainWindow.resize(600, 400);
     mainWindow.show();
-
     return app.exec();
 }
